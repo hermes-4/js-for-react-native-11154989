3b5d8d51618a353c5c95b69122373b3ae7ebed89
hermes-4 <ebenezeroforia08@gmail.com>
2024-05-19 21:53:17 +0000

    Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -3,3 +3,17 @@ const processArray = (arr) => {
         mapping % 2 === 0 ? mapping * 2 : mapping * 3
     );
 };
+
+const formatArrayStrings = (originalNames, modifiedNumbers) => {
+    if (originalNames.length !== modifiedNumbers.length) {
+        console.log("The arrays must be of the same length");
+    }
+    return originalNames.map((str, index) => {
+        const num = modifiedNumbers[index];
+        if (num % 2 === 0) {
+            return str.toUpperCase();
+        } else {
+            return str.toLowerCase();
+        }
+    });
+};
